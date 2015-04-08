707a2f1d33a3e1679e1e37a58b94bd089f873fc1
fm-labs <flowmotionlabs@gmail.com>
2015-04-08 19:20:19 +0200

    added admin prefixed routes

--- config/routes.php
+++ config/routes.php
@@ -4,6 +4,13 @@ use Cake\Routing\Router;
 Router::connect('/login', ['plugin' => 'User', 'controller' => 'Auth', 'action' => 'login']);
 Router::connect('/logout', ['plugin' => 'User', 'controller' => 'Auth', 'action' => 'logout']);
 
-Router::plugin('User', function ($routes) {
-    $routes->fallbacks();
+Router::plugin('User', function (\Cake\Routing\RouteBuilder $routes) {
+
+    $routes->prefix('admin', function (\Cake\Routing\RouteBuilder $routes) {
+        $routes->connect('/:controller', ['action' => 'index', 'prefix' => null], ['routeClass' => 'InflectedRoute']);
+        $routes->connect('/:controller/:action/*', ['prefix' => null], ['routeClass' => 'InflectedRoute']);
+        $routes->fallbacks('InflectedRoute');
+    });
+
+    $routes->fallbacks('InflectedRoute');
 });
