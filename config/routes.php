62dd30b9c0a1f55d70698832e98bbeee775b00ae
fm-labs <flowmotionlabs@gmail.com>
2015-05-25 19:51:47 +0200

    removed admin and inflected routes; added default user routes

--- config/routes.php
+++ config/routes.php
@@ -1,16 +1,14 @@
 <?php
 use Cake\Routing\Router;
 
+// User auth routes
 Router::connect('/login', ['plugin' => 'User', 'controller' => 'Auth', 'action' => 'login']);
 Router::connect('/logout', ['plugin' => 'User', 'controller' => 'Auth', 'action' => 'logout']);
 
-Router::plugin('User', function (\Cake\Routing\RouteBuilder $routes) {
+// User registration routes
+Router::connect('/register', ['plugin' => 'User', 'controller' => 'User', 'action' => 'register']);
 
-    $routes->prefix('admin', function (\Cake\Routing\RouteBuilder $routes) {
-        $routes->connect('/:controller', ['action' => 'index', 'prefix' => null], ['routeClass' => 'InflectedRoute']);
-        $routes->connect('/:controller/:action/*', ['prefix' => null], ['routeClass' => 'InflectedRoute']);
-        $routes->fallbacks('InflectedRoute');
-    });
-
-    $routes->fallbacks('InflectedRoute');
-});
+// User routes
+Router::connect('/user/:action/*', ['plugin' => 'User', 'controller' => 'User']);
+Router::connect('/user/:action', ['plugin' => 'User', 'controller' => 'User']);
+Router::connect('/user', ['plugin' => 'User', 'controller' => 'User', 'action' => 'index']);
