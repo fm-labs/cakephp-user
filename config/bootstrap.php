c8b8db7cb5eaa873dd9ec67a9595e7117300cc21
fm-labs <flowmotionlabs@gmail.com>
2016-01-17 20:52:19 +0100

    removed auto config loading from bootstrap

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,14 +1,6 @@
 <?php
 use Cake\Core\Configure;
 
-/**
- * Automatically load app's user configuration
- *
- * Copy user.default.php to your app's config folder,
- * rename to user.php and adjust contents
- */
-try {
-    Configure::load('user');
-} catch (\Exception $ex) {
-    // do nothing
+if (!Configure::read('User')) {
+    die("User Plugin not configured");
 }
