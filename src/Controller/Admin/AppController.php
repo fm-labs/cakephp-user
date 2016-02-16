c7288e81d3464a104f0ae12e494513de2c5a29b8
fm-labs <flowmotionlabs@gmail.com>
2016-02-16 18:37:55 +0100

    imported user admin MVC from Backend plugin

---
+++ src/Controller/Admin/AppController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: flow
+ * Date: 2/16/16
+ * Time: 6:32 PM
+ */
+
+namespace User\Controller\Admin;
+
+
+class AppController extends AbstractBackendController
+{
+
+}
