6f50a94b7cf24ce6d1789f007a3f83af9b54bebe
fm-labs <flowmotionlabs@gmail.com>
2016-05-20 17:09:28 +0200

    fixed missing title

--- config/backend.php
+++ config/backend.php
@@ -1,6 +1,7 @@
 <?php
 return [
     'Backend.Plugin.User.Menu' => [
+        'title' => 'User',
         'url' => ['plugin' => 'User', 'controller' => 'Users', 'action' => 'index'],
         'icon' => 'lock',
         'requireRoot' => true, // temporary access control workaround
