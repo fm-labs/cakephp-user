c69e2f0c86142f6ec9d4a857fbfef7aa99c2531f
fm-labs <flowmotionlabs@gmail.com>
2016-11-20 20:18:09 +0100

    updated backend menu entry.

--- config/backend.php
+++ config/backend.php
@@ -1,12 +1,14 @@
 <?php
 return [
     'Backend.Plugin.User.Menu' => [
-        'title' => 'User',
-        'url' => ['plugin' => 'User', 'controller' => 'Users', 'action' => 'index'],
-        'data-icon' => 'lock',
-        'requireRoot' => true, // temporary access control workaround
 
-        '_children' => [
+        'system' => [
+            [
+                'title' => 'User',
+                'url' => ['plugin' => 'User', 'controller' => 'Users', 'action' => 'index'],
+                'data-icon' => 'users',
+            ]
         ]
+
     ],
 ];
