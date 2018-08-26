dd88d4b1682dd536c16b87eec03a47943541d141
fm-labs <flowmotionlabs@gmail.com>
2018-08-26 10:00:02 +0200

    added user locale switch feature. before sending an email the user locale will be applied. after sending the locale will be reseted to the application locale

--- src/Mailer/UserMailer.php
+++ src/Mailer/UserMailer.php
@@ -17,8 +17,23 @@ use User\Model\Table\UsersTable;
  */
 class UserMailer extends Mailer
 {
+    /**
+     * User entity
+     * @var \User\Model\Entity\User
+     */
     protected $_user;
+
+    /**
+     * Locale used for email & translations
+     * @var string
+     */
     protected $_locale;
+
+    /**
+     * The original locale used by the application.
+     * @var string
+     */
+    protected $_originalLocale;
 
     /**
      * @param Email|null $email
@@ -27,22 +42,65 @@ class UserMailer extends Mailer
     {
         parent::__construct($email);
 
+        // set the default email locale
         $this->_locale = I18n::defaultLocale();
+
+        // backup the application locale
+        $this->_originalLocale = I18n::locale();
 
         if (Configure::check('User.Mailer.profile')) {
             $this->_email->profile(Configure::read('User.Mailer.profile'));
         }
     }
 
+    /**
+     * Overloaded send method
+     */
+    public function send($action, $args = [], $headers = [])
+    {
+        parent::send($action, $args, $headers);
+    }
+
+    /**
+     * Overloaded reset method.
+     * Resets temp locale switch.
+     */
+    public function reset()
+    {
+        parent::reset();
+        $this->_locale = I18n::defaultLocale();
+        //$this->_user = null;
+
+        // restore original locale
+        I18n::locale($this->_originalLocale);
+    }
+
+    /**
+     * Sets the active user for emailing
+     */
     protected function _setUser(User $user)
     {
         $this->to($user->email);
         $this->set('user', $user);
 
         $this->_user = $user;
+        $this->_originalLocale = I18n::locale();
         $this->_locale = ($user['locale']) ?: I18n::defaultLocale();
+
+        // temporarily switch to email locale
+        I18n::locale($this->_locale);
     }
 
+    /**
+     * Sets the email profile.
+     * Checks configurations in following order:
+     *
+     * User.EmailTranslation.[LOCALE].[PROFILE]
+     * User.Email.[PROFILE]
+     *
+     * @param string|array $profile
+     * @return void
+     */
     protected function _setProfile($profile)
     {
         // if profile is a string, check the static configuration in following order:
