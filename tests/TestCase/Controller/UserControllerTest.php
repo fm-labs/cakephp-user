f862a6893a1d4d1770f19a23deafdb3e788e718b
fm-labs <flowmotionlabs@gmail.com>
2019-03-10 16:11:00 +0100

    added more tests

---
+++ tests/TestCase/Controller/UserControllerTest.php
@@ -0,0 +1,172 @@
+<?php
+namespace User\Test\TestCase\Controller;
+
+use Cake\TestSuite\IntegrationTestCase;
+use User\Controller\UserController;
+
+/**
+ * User\Controller\UserController Test Case
+ */
+class UserControllerTest extends IntegrationTestCase
+{
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'plugin.user.users',
+        'plugin.user.groups',
+    ];
+
+    /**
+     * Test initialize method
+     *
+     * @return void
+     */
+    public function testInitialize()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test beforeFilter method
+     *
+     * @return void
+     */
+    public function testBeforeFilter()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test login method
+     *
+     * @return void
+     */
+    public function testLogin()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test logout method
+     *
+     * @return void
+     */
+    public function testLogout()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test index method
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test register method
+     *
+     * @return void
+     */
+    public function testRegister()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test registerGroup method
+     *
+     * @return void
+     */
+    public function testRegisterGroup()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test activate method
+     *
+     * @return void
+     */
+    public function testActivate()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test activateResend method
+     *
+     * @return void
+     */
+    public function testActivateResend()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test passwordForgotten method
+     *
+     * @return void
+     */
+    public function testPasswordForgotten()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test passwordSent method
+     *
+     * @return void
+     */
+    public function testPasswordSent()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test passwordReset method
+     *
+     * @return void
+     */
+    public function testPasswordReset()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test passwordChange method
+     *
+     * @return void
+     */
+    public function testPasswordChange()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test passwordChanged method
+     *
+     * @return void
+     */
+    public function testPasswordChanged()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test session method
+     *
+     * @return void
+     */
+    public function testSession()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
