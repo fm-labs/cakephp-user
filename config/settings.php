67e49f9ca031914dc0084cbb660fc3397e47a1c2
fm-labs <flowmotionlabs@gmail.com>
2019-04-10 16:22:32 +0200

    added user auth settings

--- config/settings.php
+++ config/settings.php
@@ -1,13 +1,24 @@
 <?php
 return [
     'Settings' => [
-        /*
-        'auth' => [
-            'settings' => []
+        'user_auth' => [
+            'settings' => [
+                'User.Login.disabled' => [
+                    'type' => 'boolean',
+                    'default' => false
+                ],
+                'User.EventLogger.enabled' => [
+                    'type' => 'boolean',
+                    'default' => false
+                ],
+            ]
         ],
-        */
         'user_signup' => [
             'settings' => [
+                'User.Signup.disabled' => [
+                    'type' => 'boolean',
+                    'default' => false
+                ],
                 'User.Signup.groupAuth' => [
                     'type' => 'boolean',
                     'default' => false
